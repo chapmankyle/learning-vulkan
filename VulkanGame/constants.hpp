f72a061fb16039c954845c88c052fb1076f8999c
Kyle <kyleichapman@gmail.com>
2020-09-07 11:29:36 +0200

    Add deviceExtensions vector

--- VulkanGame/constants.hpp
+++ VulkanGame/constants.hpp
@@ -23,6 +23,10 @@ namespace constants {
 	inline const std::vector<const char*> validationLayers{
 		"VK_LAYER_KHRONOS_validation"
 	};
+
+	inline const std::vector<const char *> deviceExtensions{
+		VK_KHR_SWAPCHAIN_EXTENSION_NAME
+	};
 }
 
 #endif // CONSTANTS_H
