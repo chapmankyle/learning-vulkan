d84b705723778e95cea55bbfd4c6f3e19a4679ff
Kyle <kyleichapman@gmail.com>
2020-10-18 13:32:31 +0200

    Update colours

--- VulkanGame/game.hpp
+++ VulkanGame/game.hpp
@@ -91,9 +91,9 @@ private:
 	bool framebufferResized = false;
 
 	const std::vector<Utils::Vertex> vertices = {
-		{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-		{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-		{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+		{{0.0f, -0.5f}, {1.0f, 0.0f, 1.0f}},
+		{{0.5f, 0.5f}, {0.0f, 1.0f, 1.0f}},
+		{{-0.5f, 0.5f}, {1.0f, 1.0f, 0.0f}}
 	};
 
 	/*
