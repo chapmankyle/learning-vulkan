9af7b9f0f808b1c76922fd827fd74153ea96fd6d
Kyle Chapman <kyleichapman@gmail.com>
2021-01-07 17:35:30 +0200

    Update path for VulkanSDK

--- VulkanGame/shaders/compile.bat
+++ VulkanGame/shaders/compile.bat
@@ -1,4 +1,4 @@
-C:\VulkanSDK\1.2.141.2\Bin32\glslc.exe shader.vert -o vert.spv
-C:\VulkanSDK\1.2.141.2\Bin32\glslc.exe shader.frag -o frag.spv
+C:\VulkanSDK\1.2.162.0\Bin32\glslc.exe shader.vert -o vert.spv
+C:\VulkanSDK\1.2.162.0\Bin32\glslc.exe shader.frag -o frag.spv
 pause
 
